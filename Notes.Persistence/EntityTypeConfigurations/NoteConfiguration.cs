c46e2cc7012f0586f8a3779f3b43064fef9c8534
Itanik <yan.xhrome@gmail.com>
2022-04-08 18:57:36 +0300

    Configure Db context.

--- Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
 
 namespace Notes.Persistence.EntityTypeConfigurations
 {
-    internal class NoteConfiguration
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
     {
+        // настройка генерации таблицы из сущности
+        void IEntityTypeConfiguration<Note>.Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+            builder.Property(note => note.Titile).HasMaxLength(250);
+        }
     }
 }
