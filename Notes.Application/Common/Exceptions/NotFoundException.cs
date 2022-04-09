6f14b7784e09ba874537c80bbcb95dc5aac94070
Itanik <yan.xhrome@gmail.com>
2022-04-09 13:32:51 +0300

    Add not hound exception.

---
+++ Notes.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Notes.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        // исключение получения сущности в БД
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found") { }
+    }
+}
