1af485c17f2d63e077b02b224a933054e6a59bb2
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-04-11 03:49:33 +0300

    Add create, update and delete methods.

--- Notes.WebApi/Controllers/NoteController.cs
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.DeleteNote;
+using Notes.Application.Notes.Commands.UpdateNote;
 using Notes.Application.Notes.Queries.GetNoteDetails;
 using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.WebApi.Models;
 using System;
 using System.Threading.Tasks;
 
@@ -8,6 +13,10 @@ namespace Notes.WebApi.Controllers
 {
     public class NoteController : BaseController
     {
+        private readonly IMapper _mapper;
+
+        public NoteController(IMapper mapper) => _mapper = mapper;
+
         [HttpGet]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
@@ -29,8 +38,46 @@ namespace Notes.WebApi.Controllers
                 UserId = UserId,
                 Id = id
             };
+
             var vm = await Mediator.Send(query);
+
             return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody]CreateNoteDto createNoteDto)
+        {
+            var createNoteCommand = _mapper.Map<CreateNoteCommand>(createNoteDto);
+            createNoteCommand.UserId = UserId;
+
+            Guid noteId = await Mediator.Send(createNoteCommand);
+
+            return Ok(noteId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody]UpdateNoteDto updateNoteDto)
+        {
+            var updateCommand = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
+            updateCommand.UserId = UserId;
+
+            await Mediator.Send(updateCommand);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var command = new DeleteNoteCommand
+            {
+                UserId = UserId,
+                Id = id
+            };
+
+            await Mediator.Send(command);
+
+            return NoContent();
         }
     }
 }
---
+++ Notes.WebApi/Models/UpdateNoteDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mapping;
+using Notes.Application.Notes.Commands.UpdateNote;
+using System;
+
+namespace Notes.WebApi.Models
+{
+    public class UpdateNoteDto : IMapWith<UpdateNoteCommand>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateNoteDto, UpdateNoteCommand>()
+                .ForMember(updateCommand => updateCommand.Id, opt => opt.MapFrom(dto => dto.Id))
+                .ForMember(updateCommand => updateCommand.Title, opt => opt.MapFrom(dto => dto.Title))
+                .ForMember(updateCommand => updateCommand.Details, opt => opt.MapFrom(dto => dto.Details));
+        }
+    }
+}
