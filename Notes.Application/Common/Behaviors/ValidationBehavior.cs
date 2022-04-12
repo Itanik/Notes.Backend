dc935d291563ac9cde0014de057c153998af21b7
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-04-12 21:20:06 +0300

    Add validation.

---
+++ Notes.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<IRequest>> validators) => _validators = validators;
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+
+            return next();
+        }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryValidator : AbstractValidator<GetNoteDetailsQuery>
+    {
+        public GetNoteDetailsQueryValidator()
+        {
+            RuleFor(getNoteDetailsQuery => getNoteDetailsQuery.Id).NotEqual(Guid.Empty);
+            RuleFor(getNoteDetailsQuery => getNoteDetailsQuery.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
